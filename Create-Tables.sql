946520b99b7b0353d4a26772f3c6acee9b19f1b2
Nitheesh Koushik G <72812956+Nitheesh-Koushik@users.noreply.github.com>
2024-04-24 12:13:03 -0400

    Update Create-Tables.sql

--- Create-Tables.sql
+++ Create-Tables.sql
@@ -1,58 +1,51 @@
-USE fatsecret;
+SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS weight_entry; 
 DROP TABLE IF EXISTS calories_burned; 
 DROP TABLE IF EXISTS food_macros; 
-DROP TABLE IF EXISTS profile_table; 
 DROP TABLE IF EXISTS food_entry;
-
-
+DROP TABLE IF EXISTS profile_table; 
+SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE profile_table (
-	profile_id INT NOT NULL auto_increment, 
-    goal_weight INT NOT NULL, 
+    profile_id INT NOT NULL auto_increment, 
+    goal_weight FLOAT NOT NULL, 
     height INT NOT NULL,
     PRIMARY KEY (profile_id)
  ); 
- 
- 
-
  CREATE TABLE weight_entry (
-	entry_id INT NOT NULL auto_increment, 
     profile_id INT NOT NULL,
     date_of_log date NOT NULL, 
-    weight INT NOT NULL, 
+    weight FLOAT NOT NULL, 
     BMR INT NOT NULL, 
-    PRIMARY KEY (entry_id), 
+    PRIMARY KEY (profile_id, date_of_log), 
     FOREIGN KEY (profile_id) REFERENCES profile_table(profile_id) 
  ); 
-
-
 CREATE TABLE calories_burned (
-	entry_id INT NOT NULL auto_increment, 
-    person_id INT NOT NULL, 
+    profile_id INT NOT NULL, 
     date_of_log DATE NOT NULL, 
     calories_burned INT NOT NULL,
-    PRIMARY KEY (entry_id), 
-    FOREIGN KEY (profile_id) REFERENCES profile_table(person_id)
+    PRIMARY KEY (profile_id, date_of_log), 
+    FOREIGN KEY (profile_id) REFERENCES profile_table(profile_id)
     );
- 
-CREATE TABLE macros (
-	food_id INT NOT NULL,
+CREATE TABLE food_macros (
+    food_id INT NOT NULL,
+    serving_id INT NOT NULL,
     food_name VARCHAR(100),
     protien FLOAT, 
     carbs FLOAT, 
     fat FLOAT, 
     calories FLOAT, 
-    PRIMARY KEY (food_id)
+    PRIMARY KEY (food_id, serving_id)
     );
- CREATE TABLE food (
-	entry_id INT NOT NULL, 
+ CREATE TABLE food_entry (
+    entry_id BIGINT NOT NULL, 
     food_id INT NOT NULL, 
-    person_id INT NOT NULL, 
+    serving_id INT NOT NULL,
+    profile_id INT NOT NULL, 
     date_of_log DATE NOT NULL,
     meal_type VARCHAR(10) NOT NULL,
     quantity FLOAT NOT NULL, 
     PRIMARY KEY (entry_id), 
-    FOREIGN KEY (profile_id) REFERENCES profile_table(person_id),
-    FOREIGN KEY (food_id) REFERENCES macros(food_id)
+    FOREIGN KEY (profile_id) REFERENCES profile_table(profile_id),
+    FOREIGN KEY (food_id) REFERENCES food_macros(food_id)
     );
